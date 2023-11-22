44e1a59c3010a779c255630bac96e808159b474a
Developer-Devyansh <devyanshyadav41@gmail.com>
2023-11-22 23:03:22 +0530

    done

--- app/page.js
+++ app/page.js
@@ -1,113 +1,126 @@
+'use client'
+import { Button, Card, Input, Skeleton } from '@nextui-org/react'
+import axios from 'axios'
 import Image from 'next/image'
+import React, { useEffect, useState } from 'react'
+import { FiWind } from "react-icons/fi";
+import { WiHumidity } from "react-icons/wi";
+import { MdCompress } from "react-icons/md";
 
-export default function Home() {
+const page = () => {
+
+  const [image, setImage] = useState('')
+  const [loader, setloader] = useState(true)
+  const [searchCity, setSearchCity] = useState('Mumbai')
+  const [cityName, setCityName] = useState(searchCity)
+  const [weatherData, setWeatherData] = useState({
+    location:'',
+    temperature:'',
+    condition:'',
+    humidity:'',
+    pressure:'',
+    description:'',
+    windSpeed:'',
+    icon:'',
+  })
+
+ useEffect(()=>{
+
+     let apiKey='08db711abbd634b509db6847d0336070'
+     
+   axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`)
+   .then(res=>{
+    console.log(res.data) 
+    console.log(res.data.weather[0].main) 
+    setWeatherData({
+      location:cityName,
+      temperature:res.data.main.temp,
+      condition:res.data.weather[0].main,
+      humidity:res.data.main.humidity,
+      pressure:res.data.main.pressure,
+      description:res.data.weather[0].description,
+      windSpeed:res.data.wind.speed,
+      icon:res.data.weather[0].icon,
+    })
+    setloader(false)
+    let locationName=res.data.name 
+    axios.get(`https://api.teleport.org/api/urban_areas/slug:${locationName.toLowerCase()}/images/`)
+    .then((res)=>{
+      let cityPhoto=res.data.photos[0].image.mobile
+      setImage(cityPhoto)
+    })
+    .catch((error)=>{
+      
+    })
+
+   })
+   .catch((error)=>{
+   })
+  
+ }, [cityName])
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+   <main className='flex items-center justify-center bg-[#2B2A4C] h-screen'>
+     <section className='md:w-9/12  w-full md:max-w-2xl md:h-96 shadow-lg  border-white border-2 bg-slate-300 md:rounded-md overflow-hidden flex md:flex-row flex-col'>
+      <div className='md:w-2/5 h-full  relative'>
+      <h3 className='absolute font-semibold text-6xl z-10 bottom-2 text-ellipsis drop-shadow-2xl text-center w-full'>{weatherData.location}</h3>
+          <Image src={image} alt='Images' width={500} height={500} className='object-cover w-full h-full'/>
+      </div>
+      <div className='flex-1 h-full bg-[#FAF6F0] space-y-5 text-black  py-3'>
+      <p className='text-center font-light text-xl'>Today Weather</p>
+
+          <div className='flex gap-3 overflow-hidden items-center justify-center'>
+            <Image src={`https://openweathermap.org/img/wn/${weatherData.icon}@2x.png`} width={500} height={500} alt='icon' className='w-32  animate-pulse duration-250'/>
+            <h2 className='text-5xl font-bold '>{Math.round(weatherData.temperature)}°C</h2>
+          </div>
+
+        <div className='flex items-center justify-around'>
+          <h2 className='flex items-center justify-center flex-col font-light'><p className='font-semibold text-xl flex items-center justify-center'>
+          <FiWind/> 
+          {weatherData.windSpeed}m/s
+          </p>
+           Wind Speed</h2>
+           <h2 className='font-light flex items-center justify-center flex-col'><p className=' font-semibold text-xl flex items-center justify-center'>
+
+          <WiHumidity/>
+           {
+            weatherData.humidity
+           }%
+          </p>
+           Humidity
+          </h2>
+          <h2 className='font-light flex items-center justify-center flex-col'><p className=' font-semibold text-xl flex items-center justify-center'>
+
+          <MdCompress />
+           {
+            weatherData.pressure
+           }hPa
+          </p>
+           Pressure
+          </h2>
         </div>
-      </div>
+        <h2 className='flex items-center justify-center'>
+          Description:  <p className='font-light'>{weatherData.description}</p>
+        </h2>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+        <span className='flex items-center justify-center p-1'>
+        <Input type="email"  classNames={{
+          inputWrapper:'h-1 p-1 px-3'
+        }}
+        endContent={
+          <Button className='h-full' onClick={()=>setCityName(searchCity)}>Search</Button>
+        } 
+        placeholder='Search by City Name'
+        onChange={(e)=>setSearchCity(e.target.value)}
+        className='border border-black rounded-md'
         />
+        
+        </span>
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+</section>
+   </main>
   )
 }
+
+export default page
--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports ={
+nextConfig,
+reactStrictMode:true,
+
+    images: {
+        domains:['images.unsplash.com', 'd13k13wj6adfdf.cloudfront.net', 'openweathermap.org']
+      },
+    }
